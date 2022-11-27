3dd422e9617f395fa69b5a6a667bec15650fa7a8
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2022-11-27 17:00:35 +0700

    update material UI and remove tailwind css

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Home from './customers/pages/Home';
+import { Routes, Route} from 'react-router-dom';
+import Layout from './customers/containers/Layout';
+import NotFound from './customers/pages/NotFound';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold italic">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Home/>} />
+        <Route path='blogs' element={<Home/>}/>
+        <Route path='contact' element={<Home/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Route>
+    </Routes>
   );
 }
 

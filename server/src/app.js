2060b42ee694c6a00c3dbf53524172d9d925b45f
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2022-12-15 15:31:25 +0700

    Cập nhật app
    Thêm api /* -> not found
    Bỏ qua cors origin

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -7,6 +6,7 @@ const morgan = require('morgan');
 
 const app = express();
 
+<<<<<<< Updated upstream
 app.use(cors({
     origin: 'http://localhost:5000',
 }));
@@ -22,15 +22,28 @@ app.use('/users',userRouter);
 app.use('/products',productRouter);
 app.use('/categories',categoryRouter);
 // app.use(express.static(path.join(__dirname, '..', 'public')));
+=======
+app.use(cors());
 
-// app.use('/v1', api);
+app.use(morgan('combined'));
+
+app.use(express.json());
+>>>>>>> Stashed changes
+
+app.use('/api/v1', api);
 
 /**
- * Not Found => index page
+ * Not Found
  */
+<<<<<<< Updated upstream
 // app.get('/*', (req, res) => {
 //     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 // });
 
 module.exports = app;
+=======
+app.get('/*', (req, res) => {
+    res.status(404).json({ error: "api not found!" });
+});
+>>>>>>> Stashed changes
 

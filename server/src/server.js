fa37e8f305b1c574969dfea8aca0918a6f153b75
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2022-12-15 15:33:02 +0700

    Cập nhật hàm kết nối

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const http = require('http');
 // const https = require('https');
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
+<<<<<<< Updated upstream
 const { mongoConnect } = require('./services/mongo')
 
 const server = http.createServer(app)
@@ -11,5 +13,18 @@ const startServer = async () => {
   server.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`)
   })
+=======
+
+const server = http.createServer(app);
+
+const { mongoConnect } = require('./services/mongo');
+
+async function startServer() {
+    await mongoConnect();
+    
+    server.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}...`);
+    })
+>>>>>>> Stashed changes
 }
 startServer();

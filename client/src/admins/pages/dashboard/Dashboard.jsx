33529d2f8d90b44a9df5042ab8e60e96b17cfa08
Nguyễn Duy Dương <81768109+duongnjas@users.noreply.github.com>
2022-12-16 22:52:02 +0700

    edit Dashboard

--- client/src/admins/pages/dashboard/Dashboard.jsx
+++ client/src/admins/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
-import { Group} from '@mui/icons-material';
-import DvrIcon from '@mui/icons-material/Dvr';
-import PaidIcon from '@mui/icons-material/Paid';
-import DevicesOtherIcon from '@mui/icons-material/DevicesOther';
+import { Group } from "@mui/icons-material";
+import DvrIcon from "@mui/icons-material/Dvr";
+import PaidIcon from "@mui/icons-material/Paid";
+import DevicesOtherIcon from "@mui/icons-material/DevicesOther";
 import {
   Avatar,
   Box,
@@ -14,19 +14,18 @@ import {
   Paper,
   Stack,
   Typography,
-} from '@mui/material';
-import React, { useEffect, useState } from 'react';
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import avt from "../../../images/user.png";
-import apiUsers from '../../apis/users.api';
-import apiProducts from '../../apis/products.api';
-import apiBills from '../../apis/bills.api';
-import apiBlogs from '../../apis/blogs.api';
+import apiUsers from "../../apis/users.api";
+import apiProducts from "../../apis/products.api";
+import apiBills from "../../apis/bills.api";
+import apiBlogs from "../../apis/blogs.api";
 import moment from "moment";
 import Chart from "./Chart";
-import AreaChartAdmin from './AreaChartAdmin';
- 
-const Dashboard= ({ setSelectedLink, link })=> {
+import AreaChartAdmin from "./AreaChartAdmin";
 
+const Dashboard = ({ setSelectedLink, link }) => {
   const [users, setUsers] = useState([]);
   const [products, setProducts] = useState([]);
   const [bills, setBills] = useState([]);
@@ -35,7 +34,6 @@ const Dashboard= ({ setSelectedLink, link })=> {
   // const [totalPage, setTotalPage] = useState(1);
   // const size = 5
 
-
   useEffect(() => {
     setSelectedLink(link);
 
@@ -43,28 +41,28 @@ const Dashboard= ({ setSelectedLink, link })=> {
       const response = await apiUsers.getAllUsers();
       // console.log(response);
       //setTotalPage(Math.ceil(response.length / size));
-      setUsers(response);    
+      setUsers(response);
     };
     getUsers();
 
     const getProducts = async () => {
       const response = await apiProducts.getAllProducts();
       //console.log(response);
-      setProducts(response);      
+      setProducts(response);
     };
     getProducts();
 
     const getBills = async () => {
       const response = await apiBills.getAllBills();
       //console.log(response);
-      setBills(response);      
+      setBills(response);
     };
     getBills();
 
     const getBlogs = async () => {
       const response = await apiBlogs.getAllBlogs();
       //console.log(response);
-      setBlogs(response);      
+      setBlogs(response);
     };
     getBlogs();
   }, []);
@@ -73,35 +71,35 @@ const Dashboard= ({ setSelectedLink, link })=> {
   //   setPage(newValue);
   // };
 
-  const TotalBills= () => {
+  const TotalBills = () => {
     let total = 0;
     bills.forEach(function (item) {
-      total+= (item.productPrice*item.productQuantity)
-    })
+      total += item.productPrice * item.productQuantity;
+    });
     return total;
-  }
-  const format= (n) => {
-    return '$'+n.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-  }
+  };
+  const format = (n) => {
+    return "$" + n.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  };
 
   return (
     <Box
       sx={{
-        display: { xs: 'flex', md: 'grid' },
-        gridTemplateColumns: 'repeat(3,1fr)',
-        gridAutoRows: 'minmax(100px, auto)',
+        display: { xs: "flex", md: "grid" },
+        gridTemplateColumns: "repeat(3,1fr)",
+        gridAutoRows: "minmax(100px, auto)",
         gap: 3,
-        textAlign: 'center',
-        flexDirection: 'column',
+        textAlign: "center",
+        flexDirection: "column",
       }}
     >
       <Paper elevation={3} sx={{ p: 3 }}>
         <Typography variant="h4">Total Users</Typography>
         <Box
           sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
           <Group sx={{ height: 100, width: 100, opacity: 0.3, mr: 1 }} />
@@ -113,12 +111,14 @@ const Dashboard= ({ setSelectedLink, link })=> {
         <Typography variant="h4">Total Products</Typography>
         <Box
           sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          <DevicesOtherIcon sx={{ height: 100, width: 100, opacity: 0.3, mr: 1 }} />
+          <DevicesOtherIcon
+            sx={{ height: 100, width: 100, opacity: 0.3, mr: 1 }}
+          />
           <Typography variant="h4">{products.length}</Typography>
         </Box>
       </Paper>
@@ -127,9 +127,9 @@ const Dashboard= ({ setSelectedLink, link })=> {
         <Typography variant="h4">Total Blogs</Typography>
         <Box
           sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
           <DvrIcon sx={{ height: 100, width: 100, opacity: 0.3, mr: 1 }} />
@@ -140,35 +140,39 @@ const Dashboard= ({ setSelectedLink, link })=> {
         <Typography variant="h4">Total Bills</Typography>
         <Box
           sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
           <PaidIcon sx={{ height: 100, width: 100, opacity: 0.3, mr: 1 }} />
           <Typography variant="h4">{format(TotalBills())} VND</Typography>
         </Box>
       </Paper>
-      <Paper elevation={3} sx={{ p: 2, gridColumn: 3, gridRow: '1/4' }}>
+      <Paper elevation={3} sx={{ p: 2, gridColumn: 3, gridRow: "1/4" }}>
         <Box>
           <Typography>Recently added Users</Typography>
           <List>
-            {users.map((user, i) => (
-              <Box key={user.id}>
-                <ListItem>
-                  <ListItemAvatar>
-                    <Avatar alt={user?.name} src={avt} />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={user?.name}
-                    secondary={`Time Created: ${moment(user?.createdAt).format(
-                      'DD-MM-YYYY ss:mm:H'
-                    )}`}
-                  /> 
-                </ListItem>
-                {i !== 3 && <Divider variant="inset" />}
-              </Box>
-            ))}
+            {users.map((user, i) => {
+              if (i < 4) {
+                return (
+                  <Box key={user.id}>
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar alt={user?.name} src={avt} />
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={user?.name}
+                        secondary={`Time Created: ${moment(
+                          user?.createdAt
+                        ).format("DD-MM-YYYY ss:mm:H")}`}
+                      />
+                    </ListItem>
+                    {i !== 3 && <Divider variant="inset" />}
+                  </Box>
+                );
+              }
+            })}
           </List>
           {/* {totalPage > 1 ? (
               <Stack spacing={2} mt="10px">
@@ -182,31 +186,35 @@ const Dashboard= ({ setSelectedLink, link })=> {
             ) : (
               <></>
             )} */}
-        </Box> 
+        </Box>
         <Divider sx={{ mt: 3, mb: 3, opacity: 0.7 }} />
         <Box>
           <Typography>Recently added Products</Typography>
-          <List >
-            {products.map((item, i) => (
-              <Box key={item.id}>
-                <ListItem>
-                  <ListItemAvatar>
-                    <Avatar
-                      alt={item?.name}
-                      //src={item?.image[0]}
-                      variant="rounded"
-                    />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={item?.name}
-                    secondary={`Price: ${format(item.price)} VND`}
-                  />
-                </ListItem>
-                {i !== 3 && <Divider variant="inset" />}
-              </Box>
-            ))}
+          <List>
+            {products.map((item, i) => {
+              if (i < 4) {
+                return (
+                  <Box key={item.id}>
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar
+                          alt={item?.name}
+                          //src={item?.image[0]}
+                          variant="rounded"
+                        />
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={item?.name}
+                        secondary={`Price: ${format(item.price)} VND`}
+                      />
+                    </ListItem>
+                    {i !== 3 && <Divider variant="inset" />}
+                  </Box>
+                );
+              }
+            })}
           </List>
-        </Box> 
+        </Box>
       </Paper>
       {/* <Paper elevation={3} sx={{p:2, gridColumn: "1/3"}}>
         <Chart/>
@@ -215,7 +223,7 @@ const Dashboard= ({ setSelectedLink, link })=> {
         <AreaChartAdmin/> 
       </Paper> */}
     </Box>
-  )
+  );
 };
 
 export default Dashboard;
